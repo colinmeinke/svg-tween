e5a11dfd48d2032a942178919e47fca87b1c4622
Colin Meinke <hello@colinmeinke.com>
2016-03-12 08:08:58 -0600

    feat: add better handling of shapes with curves

--- src/matchPoints.js
+++ src/matchPoints.js
@@ -1,3 +1,5 @@
+import arcToBezier from 'svg-arc-to-cubic-bezier';
+
 const isBetween = ( a, b, c ) => {
   if ( b.curve || c.curve ) {
     return false;
@@ -73,6 +75,17 @@ const addPoints = ( points, pointsRequired ) => {
     }
   }
 
+  if ( p.length === points.length ) {
+    const additionalPoints = pointsRequired - p.length;
+    const newPoint = { x: p[ 0 ].x, y: p[ 0 ].y };
+
+    for ( let i = 0; i < additionalPoints; i++ ) {
+      p.unshift( newPoint );
+    }
+
+    return p;
+  }
+
   return addPoints( p, pointsRequired );
 }
 
@@ -92,17 +105,91 @@ const removePoints = points => {
   return result;
 };
 
-const matchPoints = ( a, b ) => {
-  const c = removePoints( a );
-  const d = removePoints( b );
+const matchCurves = ( a, b ) => {
+  const c = [];
+  const d = [];
 
-  if ( c > d ) {
-    return [ c, addPoints( d, c.length )];
-  } else if ( d > c ) {
-    return [ addPoints( c, d.length ), d ];
+  for ( let i = 0, l = a.length; i < l; i++ ) {
+    if ( a[ i ].curve && !b[ i ].curve ) {
+      c.push( a[ i ]);
+      d.push({ ...b[ i ], curve: {
+        type: 'cubic',
+        x1: b[ i - 1 ].x,
+        y1: b[ i - 1 ].y,
+        x2: b[ i ].x,
+        y2: b[ i ].y,
+      }});
+    } else if ( b[ i ].curve && !a[ i ].curve ) {
+      d.push( b[ i ]);
+      c.push({ ...a[ i ], curve: {
+        type: 'cubic',
+        x1: a[ i - 1 ].x,
+        y1: a[ i - 1 ].y,
+        x2: a[ i ].x,
+        y2: a[ i ].y,
+      }});
+    } else {
+      c.push( a[ i ]);
+      d.push( b[ i ]);
+    }
   }
 
   return [ c, d ];
+};
+
+const convertCurvesToCubic = points => {
+  const p = [];
+
+  for ( let i = 0, l = points.length; i < l; i++ ) {
+    const point = points[ i ];
+
+    if ( point.curve && point.curve.type !== 'cubic' ) {
+      const { x: px, y: py } = points[ i - 1 ];
+      const { x: cx, y: cy } = point;
+
+      if ( point.curve.type === 'arc' ) {
+        const curves = arcToBezier({
+          px,
+          py,
+          cx,
+          cy,
+          rx: point.curve.rx,
+          ry: point.curve.ry,
+          xAxisRotation: point.curve.xAxisRotation,
+          largeArcFlag: point.curve.largeArcFlag,
+          sweepFlag: point.curve.sweepFlag,
+        });
+
+        curves.forEach(({ x1, y1, x2, y2, x, y }) => {
+          p.push({ x, y, curve: { type: 'cubic', x1, y1, x2, y2 }});
+        });
+      } else if ( point.curve.type === 'quadratic' ) {
+        const x1 = px + ( 2/3 * ( point.curve.x1 - px ));
+        const y1 = py + ( 2/3 * ( point.curve.y1 - py ));
+        const x2 = cx + ( 2/3 * ( point.curve.x1 - cx ));
+        const y2 = cy + ( 2/3 * ( point.curve.y1 - cy ));
+
+        p.push({ x: cx, y: cy, curve: { type: 'cubic', x1, y1, x2, y2 }});
+      }
+    } else {
+      p.push( point );
+    }
+  }
+
+  return p;
+};
+
+const matchPoints = ( a, b ) => {
+  let c = convertCurvesToCubic( removePoints( a ));
+  let d = convertCurvesToCubic( removePoints( b ));
+
+  if ( d.length > c.length ) {
+    c = addPoints( c, d.length );
+  } else if ( c.length > d.length ) {
+    d = addPoints( d, c.length );
+  }
+
+  return matchCurves( c, d );
 };
 
 export { addPoints, removePoints };
